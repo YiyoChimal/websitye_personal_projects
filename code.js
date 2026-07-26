fdca1a40214b5c3ad32d75dc43c7845d9fab9713
Yair <yiyochimal14@gmail.com>
2026-07-25 23:28:42 -0600

    Merge all js files to one that is located at the begining

--- code.js
+++ code.js
@@ -1,6 +1,13 @@
-function Menu(){
-    document.write(`<div class="menu-box">
-            <a href="index.html" title="Back to Main">
+function Menu(nameMenu){
+switch(nameMenu){
+    case "main":move="";break;
+    case "option_1":move="../../";break;
+    case "option_2": move="../../../";break;
+}
+
+
+            document.write(`<div class="menu-box">
+            <a href="${move}index.html" title="Back to Main">
                 <p class="menu-first">Main</p>
             </a>
             
@@ -8,16 +15,16 @@ function Menu(){
                 <details>
                     <summary>Robotics</summary>
                     <ul class="menu-second">
-                        <a href="robotics/micromouse/index.html">
+                        <a href="${move}robotics/micromouse/index.html">
                             <li>Micromouse</li>
                         </a>
-                        <a href="robotics/donkey_cars/index.html">
+                        <a href="${move}robotics/donkey_cars/index.html">
                             <li>Donkye Cars</li>
                         </a>
-                        <a href="robotics/cnc/index.html">
+                        <a href="${move}robotics/cnc/index.html">
                             <li>3 Axis CNC</li>
                         </a>
-                        <a href="robotics/robotic_arm/index.html">
+                        <a href="${move}robotics/robotic_arm/index.html">
                             <li>Robotic Arm (3 DOF)</li>
                         </a>
                     </ul>
@@ -25,13 +32,13 @@ function Menu(){
                 <details>
                     <summary>NASA Space Apss Challenges</summary>
                     <ul class="menu-second">
-                        <a href="nasa_space/2023/index.html">
+                        <a href="${move}nasa_space/2023/index.html">
                             <li>2023-Fire Management</li>
                         </a>
-                        <a href="nasa_space/2024/index.html">
+                        <a href="${move}nasa_space/2024/index.html">
                             <li>2024-Smart Agriculture</li>
                         </a>
-                        <a href="nasa_space/2025/index.html">
+                        <a href="${move}nasa_space/2025/index.html">
                             <li>2025-Air Quality Monitoring</li>
                         </a>
                     </ul>
@@ -40,23 +47,23 @@ function Menu(){
                 <details>
                     <summary>Others</summary>
                     <ul class="menu-second">
-                        <a href="others/condenser/index.html">
+                        <a href="${move}others/condenser/index.html">
                             <li>Air Humidity Condenser</li>
                         </a>
-                        <a href="others/electronic_fuse/index.html">
+                        <a href="${move}others/electronic_fuse/index.html">
                             <li>Analog Electronic Fuse</li>
                         </a>
-                        <a href="others/smart_house/index.html">
+                        <a href="${move}others/smart_house/index.html">
                             <li>Smart House Model</li>
                         </a>
-                        <a href="others/conveyor_belt/index.html">
+                        <a href="${move}others/conveyor_belt/index.html">
                             <li>Color-Sorting Conveyor belt</li>
                         </a>
-                        <a href="others/rgb_led_controller/index.html">
+                        <a href="${move}others/rgb_led_controller/index.html">
                             <li>RGB_Led_Controller</li>
                         </a>
                     </ul>
                 </details>
             </div>
         </div>`);
-}
+    }
