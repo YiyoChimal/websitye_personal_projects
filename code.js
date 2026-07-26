572e7f336f5cedecfa5cf5461f3aca7386e83710
Yair <yiyochimal14@gmail.com>
2026-07-25 20:55:34 -0600

    Added JS fuction for the menu bar to all HTML files to reduce code, delated all inecessary css files, It´ve missed to combine all js files on one to reduce files

---
+++ code.js
@@ -0,0 +1,62 @@
+function Menu(){
+    document.write(`<div class="menu-box">
+            <a href="index.html" title="Back to Main">
+                <p class="menu-first">Main</p>
+            </a>
+            
+            <div class="menu-first">
+                <details>
+                    <summary>Robotics</summary>
+                    <ul class="menu-second">
+                        <a href="robotics/micromouse/index.html">
+                            <li>Micromouse</li>
+                        </a>
+                        <a href="robotics/donkey_cars/index.html">
+                            <li>Donkye Cars</li>
+                        </a>
+                        <a href="robotics/cnc/index.html">
+                            <li>3 Axis CNC</li>
+                        </a>
+                        <a href="robotics/robotic_arm/index.html">
+                            <li>Robotic Arm (3 DOF)</li>
+                        </a>
+                    </ul>
+                </details>
+                <details>
+                    <summary>NASA Space Apss Challenges</summary>
+                    <ul class="menu-second">
+                        <a href="nasa_space/2023/index.html">
+                            <li>2023-Fire Management</li>
+                        </a>
+                        <a href="nasa_space/2024/index.html">
+                            <li>2024-Smart Agriculture</li>
+                        </a>
+                        <a href="nasa_space/2025/index.html">
+                            <li>2025-Air Quality Monitoring</li>
+                        </a>
+                    </ul>
+                </details>
+
+                <details>
+                    <summary>Others</summary>
+                    <ul class="menu-second">
+                        <a href="others/condenser/index.html">
+                            <li>Air Humidity Condenser</li>
+                        </a>
+                        <a href="others/electronic_fuse/index.html">
+                            <li>Analog Electronic Fuse</li>
+                        </a>
+                        <a href="others/smart_house/index.html">
+                            <li>Smart House Model</li>
+                        </a>
+                        <a href="others/conveyor_belt/index.html">
+                            <li>Color-Sorting Conveyor belt</li>
+                        </a>
+                        <a href="others/rgb_led_controller/index.html">
+                            <li>RGB_Led_Controller</li>
+                        </a>
+                    </ul>
+                </details>
+            </div>
+        </div>`);
+}
